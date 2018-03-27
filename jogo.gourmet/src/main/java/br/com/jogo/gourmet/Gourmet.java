2c4ddc2eb58607d0aa3439341922751389494fd1
Andre Fabiano Lermen <lermen.andrefabiano@gmail.com>
2018-03-27 11:30:18 -0300

    Jogo Gourmet

---
+++ jogo.gourmet/src/main/java/br/com/jogo/gourmet/Gourmet.java
@@ -0,0 +1,18 @@
+package br.com.jogo.gourmet;
+
+import java.io.Serializable;
+
+/**
+ * Classe mestre para iniciar o jogo gourmet
+ * 
+ * @author andre.lermen
+ *
+ */
+public class Gourmet implements Serializable {
+
+	private static final long serialVersionUID = -8331285322591800101L;
+
+	public static void main(String[] args) {
+		new CriarGourmet();
+	}
+}
